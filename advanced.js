173373f39636af7350318049114645828a632ff8
stevencowell <steven.cowell@det.nsw.edu.au>
2025-07-31 18:17:08 -0700

    Fix Advanced sheet submission

--- advanced.js
+++ advanced.js
@@ -20,6 +20,7 @@ async function submitAdv(e, week) {
 
   try {
     await fetch(SHEET_WEBAPP_URL, {
+      mode: 'no-cors',
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
